e8783d628c13f1e35b72fb0e2ef4fb174cb5c761
Furqan Anwar <choudharyfurqan0@gmail.com>
2024-09-07 14:51:32 +0500

    Implemented connection to postgres databse

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,26 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { SequelizeModule } from '@nestjs/sequelize';
+import { UserModule } from './users/user.module';
+import * as dotenv from 'dotenv';
+
+dotenv.config();
 
 @Module({
-  imports: [],
+  imports: [
+    SequelizeModule.forRoot({
+      dialect: 'postgres',
+      host: process.env.DATABASE_HOST,
+      port: +process.env.DATABASE_PORT,
+      username: process.env.DATABASE_USER,
+      password: process.env.DATABASE_PASSWORD,
+      database: process.env.DATABASE_NAME,
+      autoLoadModels: true,
+      synchronize: true
+    }),
+    UserModule
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
